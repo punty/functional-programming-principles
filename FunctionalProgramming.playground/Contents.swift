6be49f416f2834f685c0d22526844dafa52dff8b
Francesco Puntillo <francesco.puntillo@gmail.com>
2019-05-22 15:30:07 +0100

    First Commit [WIP]

---
+++ FunctionalProgramming.playground/Contents.swift
@@ -0,0 +1,123 @@
+import Foundation
+
+//Functional Programming Principles in Swift
+
+//1.Evaluation Strategy
+//All the evaluation does is reducing an expression to a value
+//Can express every algorithm (if not side effect) -> example C++ (has a side effect on the variable c so can't be express as substitution we need a store)
+
+
+//call-by-value -> first resolve arguments then function <- this is used by default
+
+//call-by-name -> first resolve function then arguments <- Autoclosure
+
+//http://www.russbishop.net/swift-call-by-name
+let square: (Double) -> Double = { $0 * $0 }
+square(2.0)
+
+let sumOfSquare: (Double, Double) -> Double  = {
+    return square($0) + square($1)
+}
+
+//substitution model
+sumOfSquare(3, 2+2)
+sumOfSquare(3, 4)
+square(3) + square(4)
+3 * 3 + square(4)
+9 + square(4)
+9 + 4 * 4
+9 + 16
+25
+
+//Lambda calculs
+
+//Tail recursion
+
+func sqrt(x: Double, guess: Double) -> Double {
+    func isGuessValid(_ guess: Double) -> Bool {
+        return abs(x * x - guess) / x < 0.001
+    }
+    
+    func improveGuess(_ guess: Double) -> Double {
+        return (guess + x / guess) / 2.0
+    }
+    
+    func sqrtIter(_ guess: Double) -> Double {
+        if isGuessValid(guess) {
+            return guess
+        } else {
+            return sqrtIter(improveGuess(guess))
+        }
+    }
+    return sqrtIter(1.0)
+}
+
+sqrt(10)
+
+//Functional form of a loop (Tail Recursion)
+func gcd(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a: gcd(b,a % b)
+}
+
+//Evaluation
+gcd(14,21)
+gcd(14,21)
+gcd(21, 14)
+gcd(14, 7)
+gcd(7, 0)
+7
+
+func factorial(_ n: Int) -> Int {
+    precondition(n >= 0)
+    if n == 0 {
+        return 1
+    }
+    
+    return n * factorial(n - 1)
+}
+
+factorial(4)
+
+//Evaluation
+factorial (4)
+4 * factorial(3)
+4 * (3 * factorial(2))
+4 * (3 * (2 * factorial(1)))
+4 * (3 * (2 * (1 * factorial(0))))
+4 * (3 * (2 * (1 * 1)))
+24
+
+func factorialTailRec(_ n: Int) -> Int {
+    precondition(n >= 0)
+    func factorialIter(_ n: Int, _ acc: Int) -> Int {
+        if n == 0 {
+            return acc
+        } else {
+            return factorialIter(n-1, acc * n)
+        }
+    }
+    return factorialIter(n, 1)
+}
+factorialTailRec(4)
+
+/*
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+*/
+
+func pascalTriangle(col: Int, row: Int) -> Int {
+    func triangle(col: Int, row: Int) -> Int {
+        precondition(row >= 0 && col >= 0 && col <= row)
+        if col == row || col == 0 {
+            return 1
+        } else {
+            return triangle(col: col-1, row: row-1) + triangle(col: col, row: row-1)
+        }
+    }
+    return triangle(col: col, row: row)
+}
+pascalTriangle(col: 2, row: 4)
+
